4eedb70ab877bc20671cce71bb479c9a237d20e4
esergly <kievbsm@gmail.com>
2017-10-02 02:13:38 +0300

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,63 @@
+package HomeWorkLessonOneTaskTwo;
+
+public class Triangle {
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c) {
+        super();
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Triangle() {
+        super();
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                '}';
+    }
+
+    public double Square(double a, double b, double c) {
+        double s;
+//   check if this triangle has correct size
+        if ((a + b) > c && (a + c) > b && (c + b) > a) {
+//   calculate the triangle Square by Geron's formula
+            s = Math.sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c));
+        } else {
+            s = 0;
+        }
+        return s;
+    }
+}
