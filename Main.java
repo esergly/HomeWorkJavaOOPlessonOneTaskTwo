5f4ab4c61fc250f7b087dd4e385ff332cd8c82b9
esergly <kievbsm@gmail.com>
2017-10-02 17:57:01 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,16 +7,21 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("** Please enter the value of triangle sides :");
-        System.out.println("The side a is equal: ");
-        double x = Double.parseDouble(bufferedReader.readLine());
-        System.out.println("The side b is equal: ");
-        double y = Double.parseDouble(bufferedReader.readLine());
-        System.out.println("The side c is equal: ");
-        double z = Double.parseDouble(bufferedReader.readLine());
-
-        Triangle triangle = new Triangle(x, y, z);
+        // Блок для случая, когда надо считать площадь треугольников по неизвестным заранее входным данным
+        //BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //System.out.println("** Please enter the value of triangle sides :");
+        //System.out.println("The side a is equal: ");
+        //double x = Double.parseDouble(bufferedReader.readLine());
+        //System.out.println("The side b is equal: ");
+        //double y = Double.parseDouble(bufferedReader.readLine());
+        //System.out.println("The side c is equal: ");
+        //double z = Double.parseDouble(bufferedReader.readLine());
+        //Triangle triangle = new Triangle(x, y, z);
+        
+        Triangle triangle = new Triangle(5, 6, 7);
+        Triangle triangle = new Triangle(11, 2, 8);
+        Triangle triangle = new Triangle(56, 112, 56);
+        
 
         if (triangle.Square(x, y, z) > 0) {
             System.out.println("The Square of this triangle is: " + triangle.Square(x, y, z));
